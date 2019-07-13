a150aab0943f0a0206921bb9754e664a95dce573
Iago <iagofalcom@hotmail.com>
2019-07-13 17:42:33 -0300

    Criando Animação no Flutter Logo

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Animações Flutter',
       debugShowCheckedModeBanner: false,
+      home: LogoApp(),
     );
   }
 }
@@ -18,10 +19,45 @@ class LogoApp extends StatefulWidget {
   _LogoAppState createState() => _LogoAppState();
 }
 
-class _LogoAppState extends State<LogoApp> {
-  
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin{
+
+  AnimationController controller;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    animation.addListener(() {
+      setState(() {
+
+      });
+    });
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Container(
+        height:  animation.value,
+        width: animation.value,
+        child:FlutterLogo(
+        ),
+      ),
+    );
   }
 }
